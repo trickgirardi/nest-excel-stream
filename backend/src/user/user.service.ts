47ac37517ccf69b2790aac2c5ffa993cf1f3a66d
Patrick Girardi <dev.patrickgirardi@gmail.com>
2025-06-10 22:14:48 -0300

    refactor: remove post and user CRUD APIs, keep only Excel export endpoint functionality

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -6,14 +6,6 @@ import { User, Prisma } from '@prisma/client';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async user(
-    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-  ): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: userWhereUniqueInput,
-    });
-  }
-
   async users(params: {
     skip?: number;
     take?: number;
@@ -28,29 +20,6 @@ export class UsersService {
       cursor,
       where,
       orderBy,
-    });
-  }
-
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    });
-  }
-
-  async updateUser(params: {
-    where: Prisma.UserWhereUniqueInput;
-    data: Prisma.UserUpdateInput;
-  }): Promise<User> {
-    const { where, data } = params;
-    return this.prisma.user.update({
-      data,
-      where,
-    });
-  }
-
-  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.delete({
-      where,
     });
   }
 }
